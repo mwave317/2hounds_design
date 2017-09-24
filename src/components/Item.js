9e6e717cc53394b92b12534ac63adee6311c0ea0
Ken Coppola <mwave317@gmail.com>
2017-09-23 23:25:13 -0400

    Uploading files with Embeded video

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ReactPlayer from 'react-player';
 export default class Item extends Component {
   constructor(props) {
     super(props);
@@ -101,8 +101,12 @@ export default class Item extends Component {
     const product = this.state.items;
     return (
       <div>
-
-        <p>{product.name[0]}</p>
+        <ReactPlayer controls='true' url='https://www.youtube.com/watch?time_continue=2&v=FagQYmTE6AA'/>
+        <p>{product.name}</p>
+        <form>
+        <input type="text" placeholder="Enter your e-mail"/>
+        <input type="button" value="Submit"/>
+      </form>
       </div>
     );
 
